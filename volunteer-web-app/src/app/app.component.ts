9ea542c59b8e948418d6e2ba249d862b5e9132be
allaman konate <allaman.konate@techwave.net>
2024-09-14 03:07:06 -0500

    working on user registration form

--- volunteer-web-app/src/app/app.component.ts
+++ volunteer-web-app/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { AdminComponent } from "./user/admin/admin.component";
 import { LoginComponent } from "./pages/login/login.component";
 import { RegistrationComponent } from "./pages/registration/registration.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AdminComponent, LoginComponent, RegistrationComponent],
+  imports: [RouterOutlet, AdminComponent, LoginComponent, RegistrationComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
