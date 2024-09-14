2f5e70f394a4848f85c1a5e1897d3c7155cf31d3
allaman konate <allaman.konate@techwave.net>
2024-09-14 00:15:35 -0500

    initial user registration styling

--- volunteer-web-app/src/app/app.component.ts
+++ volunteer-web-app/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdminComponent } from "./user/admin/admin.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { RegistrationComponent } from "./pages/registration/registration.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AdminComponent, LoginComponent, RegistrationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- volunteer-web-app/src/app/pages/registration/registration.component.ts
+++ volunteer-web-app/src/app/pages/registration/registration.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [MdbFormsModule,
+    FormsModule,
+    ReactiveFormsModule,MdbCheckboxModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
