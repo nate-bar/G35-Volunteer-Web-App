881f0566e27fa84088a5db412f64d8c13045a2a9
Kian-hash <arcadeecho@gmail.com>
2024-10-24 18:22:20 -0500

    chore: connected volunteer matching to db

--- server/config.py
+++ server/config.py
@@ -1,13 +1,16 @@
-# config.py
+import secrets
 
 class Config:
     # Flask-Mail configuration
-    MAIL_SERVER = 'smtp.gmail.com'  
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
-    MAIL_USERNAME = 'allamankonate@gmail.com'  
-    MAIL_PASSWORD = 'nrpl tdts mrvy jrxq'  
+    MAIL_USERNAME = 'allamankonate@gmail.com'
+    MAIL_PASSWORD = 'nrpl tdts mrvy jrxq'
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_DEFAULT_SENDER = 'donot@gmail.com'  # Default sender email address
+    MAIL_DEFAULT_SENDER = 'donotreply@gmail.com' 
 
-    
+    SECRET_KEY = secrets.token_hex(16)
+    SECURITY_PASSWORD_SALT = 'some_random_salt'
+
+    MONGO_URI = 'mongodb://localhost:27017/volunteer_management'
